52358f8ce921849b188bf93cda1dfe9036690a7c
AmanH41 <aman.ali789@gmail.com>
2025-04-21 22:49:54 -0600

    added stuff to roomPage

--- vite-project/src/RoomPage.jsx
+++ vite-project/src/RoomPage.jsx
@@ -3,13 +3,11 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import { leaveRoom } from "./AgoraFunc"; 
 
-
 import "./RoomPage.css";
 import hamburgerIcon from './assets/hamburgermenu.png';
 import timerIcon from './assets/timer.png';
 import arrowIcon from './assets/arrow.png';
-import xIcon from './assets/x.png';
-
+import xIcon from './assets/X.png';
 
 import AgoraRTC from "agora-rtc-sdk-ng";
 
@@ -26,7 +24,6 @@ import hair7 from './assets/hair/hair7.png';
 
 function RoomPage() {
   console.log("RoomPage rendered");
-
   const location = useLocation();
   const navigate = useNavigate();
   const params = new URLSearchParams(location.search);
